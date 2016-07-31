95d64f1b4169591b0a2164b97ecda8b84fb4a489
Hussamuddin Nasir <nasir@netlab.uky.edu>
2016-07-31 11:17:07 -0400

    Update common.py

--- Contents/Code/common.py
+++ Contents/Code/common.py
@@ -1,7 +1,7 @@
 ################################################################################
 TITLE = L('Title')
 VERSION = '0.06' # Release notation (x.y - where x is major and y is minor)
-GITHUB_REPOSITORY = 'coder-alpha/DesiTV.bundle'
+GITHUB_REPOSITORY = 'hussamnasir/DesiTV.bundle'
 PREFIX = "/video/desitv"
 ################################################################################
 
