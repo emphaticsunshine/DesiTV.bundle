6044e3e41c25fb2c28724537f0579b7c9467b34a
Hussamuddin Nasir <nasir@netlab.uky.edu>
2016-07-31 10:59:03 -0400

    Update __init__.py

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,8 +1,6 @@
-# DesiTV Plex Plugin Version 1.2
+# DesiTV Plex Plugin Version 1.2h1
 import common
 import desirulez
-import apnitv
-import desitvforum
 import updater
 import desitvbox
 
@@ -32,8 +30,6 @@ def MainMenu():
   oc = ObjectContainer()
   oc.add(DirectoryObject(key=Callback(desitvbox.ChannelsMenu, url=desitvbox.SITEURL), title=desitvbox.SITETITLE, thumb=R(desitvbox.SITETHUMB)))
   oc.add(DirectoryObject(key=Callback(desirulez.TypeMenu, url=desirulez.SITEURL), title=desirulez.SITETITLE, thumb=R(desirulez.SITETHUMB)))
-  oc.add(DirectoryObject(key=Callback(apnitv.ChannelsMenu, url=apnitv.SITEURL), title=apnitv.SITETITLE, thumb=R(apnitv.SITETHUMB)))
-  oc.add(DirectoryObject(key=Callback(desitvforum.TypeMenu, url=desitvforum.SITEURL), title=desitvforum.SITETITLE, thumb=R(desitvforum.SITETHUMB)))
   oc.add(DirectoryObject(key = Callback(updater.menu, title='Update Plugin'), title = 'Update Plugin', thumb = R(ICON_UPDATE)))
   oc.add(PrefsObject(title = 'Preferences', thumb = R(ICON_PREFS)))
   return oc
